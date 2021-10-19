d7b567a4ac0ad9ddcd46040bb32f52144592173b
Angel Cornejo <self.angelcornejo@gmail.com>
2021-10-19 01:22:08 -0700

    Adds the four grids to the screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
 import Grid from './components/Grid';
 import './styles/normalize.css';
+import './styles/App.css';
 
 const App = () => {
-  const x = <Grid player={1} name="home" />;
-  return x;
+  const x = 1;
+  return (
+    <main>
+      <Grid player={1} name="enemy" />
+      <Grid player={1} name="friendly" />
+      <Grid player={2} name="enemy" />
+      <Grid player={2} name="friendly" />
+    </main>
+  );
 };
 
 export default App;
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Tile from './Tile';
+import '../styles/Grid.css';
 
 const Grid = (props) => {
   const { player, name } = props;
+
+  let tileSet = [];
+  for (let i = 0; i < 10; i += 1) {
+    for (let j = 0; j < 10; j += 1) {
+      tileSet = tileSet.concat({ i, j });
+    }
+  }
   return (
-    <div className="grid">
-      <Tile type="" />
+    <div className={`grid ${name} player${player}`}>
+      {tileSet.map((tile) => (
+        <Tile type="" />
+      ))}
     </div>
   );
 };
