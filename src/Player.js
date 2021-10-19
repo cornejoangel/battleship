38fe5a41066fccd3b1c846e8ecce7f6b58b4860f
Angel Cornejo <self.angelcornejo@gmail.com>
2021-10-18 23:55:19 -0700

    Reduces the role of Player to just randomAttacking

--- src/Player.js
+++ src/Player.js
@@ -1,21 +1,11 @@
 const Player = () => {
-  let turnStatus = true;
-  const isTurn = () => turnStatus;
-
   const randomAttack = () => {
     const x = Math.floor(Math.random() * 10);
     const y = Math.floor(Math.random() * 10);
     return { x, y };
   };
 
-  const changeTurn = () => {
-    if (turnStatus) {
-      return (turnStatus = false);
-    }
-    return (turnStatus = true);
-  };
-
-  return { isTurn, randomAttack, changeTurn };
+  return { randomAttack };
 };
 
 export default Player;
--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -6,24 +6,7 @@ test('A player exists', () => {
   expect(p).toBeDefined();
 });
 
-test('A Player knows if it is their turn', () => {
-  expect(p.isTurn()).toBe(true);
-});
-
-// not sure players need to be able to do this
-// test('A Player is able to make an attack', () => {
-//   expect(p.attack({ x: 1, y: 1 })).toBeDefined();
-// });
-
 test('A Player is able to make a random attack', () => {
   expect(p.randomAttack()).toBeDefined();
   console.log(p.randomAttack());
-});
-
-test('A Player is able to have their turn status changed', () => {
-  expect(p.isTurn()).toBe(true);
-  p.changeTurn();
-  expect(p.isTurn()).toBe(false);
-  p.changeTurn();
-  expect(p.isTurn()).toBe(true);
 });
