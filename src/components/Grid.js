c224c60082c21798f73d9589f2669166127b7eec
Angel Cornejo <self.angelcornejo@gmail.com>
2021-10-20 17:22:16 -0700

    Misses are represented on the enemy board

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -20,21 +20,68 @@ const Grid = (props) => {
     }
   }
 
+  /*
+  Grid's conditional render:
+
+  There are two players and each player has two kinds of grids
+    Their 'friendly' grid
+    Their 'enemy' grid
+
+  There are also 4 types of tile - 'ship', 'miss', 'hit', '' (blank)
+  
+  A player's friendly grid shows their own ships and the result of any attacks the enemy has made (all tile types).
+
+  A player's enemy grid shows only the results of their own attacks (no ship tiles).
+  Furthermore, when a player makes an attack, they do so by clicking on their enemy grid
+
+  So enemy grids need:
+    to show tile types but...
+    to NOT show ship types
+    to pass along the attack function
+  
+  And friendly grids only need:
+    to show all tile types
+
+  With all that in mind, lets address the following block of code:
+  It just checks if it is making a friendly or enemy board
+  Friendly boards don't get the attack function
+  Enemy boards have their 'ship' tiles changed to '' (blank) tiles
+  Unfortunately that has to happen in an ugly if/else block inside of the mapping
+  */
+
   let grid = null;
   if (name === 'enemy') {
     grid = (
       <div className={`grid ${name} player${player}`}>
-        {tiles.map((tile) => (
-          <Tile
-            type=""
-            x={tile.x}
-            y={tile.y}
-            name={name}
-            player={player}
-            key={`${tile.x}${tile.y}`}
-            attack={attack}
-          />
-        ))}
+        {tiles.map((tile) => {
+          let t = null;
+          if (tile.type === 'ship') {
+            t = (
+              <Tile
+                type=""
+                x={tile.x}
+                y={tile.y}
+                name={name}
+                player={player}
+                key={`${tile.x}${tile.y}`}
+                attack={attack}
+              />
+            );
+          } else {
+            t = (
+              <Tile
+                type={tile.type}
+                x={tile.x}
+                y={tile.y}
+                name={name}
+                player={player}
+                key={`${tile.x}${tile.y}`}
+                attack={attack}
+              />
+            );
+          }
+          return t;
+        })}
       </div>
     );
   } else {
