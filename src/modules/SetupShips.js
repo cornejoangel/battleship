b00fe847f01806ad9b779f532fc4aac5cafc1725
Angel Cornejo <self.angelcornejo@gmail.com>
2021-11-03 00:44:37 -0700

    Adds all of the ships to SetupShips

--- src/modules/SetupShips.js
+++ src/modules/SetupShips.js
@@ -1,7 +1,10 @@
 const SetupShips = () => {
   const ships = [
-    { x: -1, y: -1, length: 2, orientation: 'horizontal', model: 'destroyer' },
-    { x: -1, y: -1, length: 3, orientation: 'vertical', model: 'submarine' },
+    { x: -1, y: -1, length: 2, orientation: 'horizontal', model: 'patrol' },
+    { x: -1, y: -1, length: 3, orientation: 'horizontal', model: 'submarine' },
+    { x: -1, y: -1, length: 3, orientation: 'horizontal', model: 'destroyer' },
+    { x: -1, y: -1, length: 4, orientation: 'horizontal', model: 'battleship' },
+    { x: -1, y: -1, length: 5, orientation: 'horizontal', model: 'carrier' },
   ];
 
   return ships;
