89777266208908c833ffd6fe3c3bdc73db96ed05
Angel Cornejo <self.angelcornejo@gmail.com>
2021-11-04 00:09:49 -0700

    Starting ship orientations are random

--- src/modules/SetupShips.js
+++ src/modules/SetupShips.js
@@ -1,10 +1,42 @@
 const SetupShips = () => {
+  // the ship orientations are randomly determined
+  const orientations = ['horizontal', 'vertical'];
   const ships = [
-    { x: -1, y: -1, length: 2, orientation: 'horizontal', model: 'patrol' },
-    { x: -1, y: -1, length: 3, orientation: 'horizontal', model: 'submarine' },
-    { x: -1, y: -1, length: 3, orientation: 'horizontal', model: 'destroyer' },
-    { x: -1, y: -1, length: 4, orientation: 'horizontal', model: 'battler' },
-    { x: -1, y: -1, length: 5, orientation: 'horizontal', model: 'carrier' },
+    {
+      x: -1,
+      y: -1,
+      length: 2,
+      orientation: orientations[Math.round(Math.random())],
+      model: 'patrol',
+    },
+    {
+      x: -1,
+      y: -1,
+      length: 3,
+      orientation: orientations[Math.round(Math.random())],
+      model: 'submarine',
+    },
+    {
+      x: -1,
+      y: -1,
+      length: 3,
+      orientation: orientations[Math.round(Math.random())],
+      model: 'destroyer',
+    },
+    {
+      x: -1,
+      y: -1,
+      length: 4,
+      orientation: orientations[Math.round(Math.random())],
+      model: 'battler',
+    },
+    {
+      x: -1,
+      y: -1,
+      length: 5,
+      orientation: orientations[Math.round(Math.random())],
+      model: 'carrier',
+    },
   ];
 
   return ships;
