629c45c25c472daa12d9833a479a8e8b1de69bf5
Angel Cornejo <self.angelcornejo@gmail.com>
2021-12-17 18:48:49 -0700

    Adds rest of text to info, styles text

--- src/components/InfoButton.js
+++ src/components/InfoButton.js
@@ -37,8 +37,28 @@ const InfoButton = () => {
           then that ship is sunk. The first player to sink all of their
           opponent's ships wins.
         </p>
-        <h3>How to Play</h3>
-        <h3>Your Opponent</h3>
+        <h3 className="info-left">How to Play</h3>
+        <p className="info-left">
+          First you must place your ships. You can drag them from their starting
+          tray (on the right) over to the grid on the left by dragging their
+          lighter colored end-tiles. You can also opt to use the appropriately
+          labeled "randomize ships" button. Rotating ships is done by clicking
+          on any part of a ship - provided the ship has clearance to rotate.
+        </p>
+        <p className="info-left">
+          Once the game begins you simply click on any tile from your enemy's
+          grid to attack that tile. Your opponent will immediately counterattack
+          and the result of both attacks will be shown below the grid. If you
+          attempt to make a duplicate move, the game will reject it and wait for
+          a new move. The game will end once all of either player's ships have
+          been sunk. You may press the reset button at any time to start over.
+        </p>
+        <h3 className="info-right">Your Opponent</h3>
+        <p className="info-left">
+          This game of Battleship is played against an AI opponent. The AI knows
+          when it gets a hit and will probe the surrounding area trying to sink
+          the rest of your ship. Don't underestimate your opponent!
+        </p>
         <button type="button" className="close-info" onClick={closeInfo}>
           close
         </button>
