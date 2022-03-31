7bbd61dab8302630e1e014cd993d49827c15cd2d
Angel Cornejo <self.angelcornejo@gmail.com>
2022-03-30 17:22:28 -0700

    Changes shiptiles to divs to work on firefox

--- src/components/ShipTile.js
+++ src/components/ShipTile.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/Tile.css';
@@ -7,13 +8,15 @@ const ShipTile = (props) => {
   const { x, y, rotateShip, model, front } = props;
   let tile = null;
   tile = (
-    <button
+    <div
       type="button"
+      role="button"
+      tabIndex={0}
       onClick={(e) => rotateShip(e, model)}
       className={`tile ship ship-tile ${front ? 'front' : 'back'}`}
     >
       {/* {x}, {y} */}
-    </button>
+    </div>
   );
 
   return tile;
